d3ef7e8843a56675e04ec3b3c614aec131e96a8b
crtituana <crtituana@gmail.com>
2020-11-05 01:05:23 -0500

    controladores actualizados

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -17,7 +17,26 @@ class CustomerController extends Controller
     public function postCustomer(Request $request){
         $data = $request->json()->all();
         $customer = Customer::create([
-            'state'=> $data['state'],
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>$data['password'],
+            'address_1'=>$data['address_1'],
+            'address_2'=>$data['address_2'],
+            'city'=>$data['city'],
+            'region'=>$data['region'],
+            'postal_code'=>$data['postal_code'],
+            'shipping_region_id'=>$data['shipping_region_id'],
+            'mob_phone'=>$data['mob_phone'],
+            'country_id'=>$data['country_id'],
+            'status_id'=> $data['status_id'],
+            'key'=> $data['key'],
+            'created_on'=> $data['created_on'],
+            'domain'=> $data['domain'],
+            'time_zone'=> $data['time_zone'],
+            'attemps'=> $data['attemps'],
+            'locked_on'=> $data['locked_on'],
+            'telegram_id'=> $data['telegram_id'],
+            'state'=>$data['state'],
         ]);
         return response()->json($customer, 201);
     }
@@ -25,6 +44,25 @@ class CustomerController extends Controller
         $data = $request->json()->all();
         $customer = Customer::findOrFail($data['id']);
         $response = $customer->update([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>$data['password'],
+            'address_1'=>$data['address_1'],
+            'address_2'=>$data['address_2'],
+            'city'=>$data['city'],
+            'region'=>$data['region'],
+            'postal_code'=>$data['postal_code'],
+            'shipping_region_id'=>$data['shipping_region_id'],
+            'mob_phone'=>$data['mob_phone'],
+            'country_id'=>$data['country_id'],
+            'status_id'=> $data['status_id'],
+            'key'=> $data['key'],
+            'created_on'=> $data['created_on'],
+            'domain'=> $data['domain'],
+            'time_zone'=> $data['time_zone'],
+            'attemps'=> $data['attemps'],
+            'locked_on'=> $data['locked_on'],
+            'telegram_id'=> $data['telegram_id'],
             'state'=> $data['state'],
         ]);
         return response()->json($customer, 201);
